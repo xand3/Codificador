339ac07f31aaa18a5a60812679eafbe60123b7da
Alexandre Bastos <xande.bastos321@gmail.com>
2022-05-19 23:22:42 -0300

    Finalizado o Layout

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let selecionar = document.querySelector("select")
+let cCesar = document.getElementById("cesar")
+let base = document.getElementById("base")
+let codificar = document.getElementById("codificar")
+let descodificar = document.getElementById("descodificar")
+let selected = document.getElementById("selected")
+
+
+
+function select() {
+	
+}
